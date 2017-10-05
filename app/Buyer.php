7513db7d527df79862fdf84ed249ef497596ec94
at-hieuvo <hieupr9695@gmail.com>
2017-10-05 16:30:50 +0700

    Implementing The Relationships Between Models

--- app/Buyer.php
+++ app/Buyer.php
@@ -2,7 +2,12 @@
 
 namespace App;
 
+use App\Transaction;
+
 class Buyer extends User
 {
-    //
+    public function transaction()
+    {
+        return $this->hasMany(Transaction::class);
+    }
 }
