d0f53aa46bd6ac865a0628b96bb0bb55749d9a1d
at-hieuvo <hieupr9695@gmail.com>
2017-10-09 14:04:11 +0700

    using implict model binding

--- app/Http/Controllers/User/UserController.php
+++ app/Http/Controllers/User/UserController.php
@@ -60,9 +60,8 @@ class UserController extends Apicontroller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function show($id)
+    public function show(User $user)
     {
-        $user = User::findOrFail($id);
         return $this->showOne($user);
     }
 
@@ -84,10 +83,8 @@ class UserController extends Apicontroller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, $id)
+    public function update(Request $request, User $user)
     {
-        $user = User::findOrFail($id);
-
         $rules = [
             'email' => 'email|unique:users, email,' . $user->id,
             'password' => 'min:6|confirmed',
@@ -129,9 +126,8 @@ class UserController extends Apicontroller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function destroy($id)
+    public function destroy(User $user)
     {
-        $user = User::findOrFail($id);
         $user->delete();
         return $this->showOne($user);
     }
