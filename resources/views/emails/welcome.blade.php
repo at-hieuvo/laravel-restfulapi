78353a9bccad81ad1a778019bd1ef68e3472dc82
at-hieuvo <hieupr9695@gmail.com>
2017-10-13 10:09:40 +0700

    using markdown maiable

--- resources/views/emails/welcome.blade.php
+++ resources/views/emails/welcome.blade.php
@@ -1,3 +1,11 @@
-Hello {{ $user->name }}
-Thank you for create account.Please verify your mail using this link:
-{{ route('verify', $user->verification_token) }}
+@component('mail::message')
+# Hello {{ $user->name }}
+Thank you for create account.Please verify your mail using this button:
+
+@component('mail::button', ['url' =>  route('verify', $user->verification_token) ])
+Verify Account
+@endcomponent
+
+Thanks,
+{{ config('app.name') }}
+@endcomponent
